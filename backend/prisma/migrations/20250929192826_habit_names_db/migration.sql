dbce3184df207b8fe74e1d0134992d1e0455b49f
annielymariah <annielyms@gmail.com>
2025-09-29 16:30:21 -0300

    feat: create docker-compose & generate migrations

---
+++ backend/prisma/migrations/20250929192826_habit_names_db/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."names" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "names_pkey" PRIMARY KEY ("id")
+);
